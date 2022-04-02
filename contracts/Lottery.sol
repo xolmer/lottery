0f9e6c7314fb4f175530e26ab3b0f9e1246e97ef
xolmer <kaveh.aidivandi@gmail.com>
2022-04-02 21:40:58 +0430

    lottery

---
+++ contracts/Lottery.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+//Lottery Contract
+contract Lottery {
+
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+    function enter() public payable {
+        require(msg.value > 0.001 ether);
+
+        players.push(msg.sender);
+    }
+
+    //Random number generator "Not that random"
+    function random() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players)));
+    }
+
+    // Pick the winner
+    function pickWinner() public restricted{
+        uint index = random() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    //Only the manager can pick the winner
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+}
