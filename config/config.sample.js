ed1aa766a59d822b255954ad56ca062c7763711f
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-05-02 16:46:29 +0100

    config fix

--- config/config.sample.js
+++ config/config.sample.js
@@ -2,5 +2,4 @@
 const mnemonicKeys = '';
 //Sign hup here: https://infura.io/signup
 const networkURL = '';
-module.exports = mnemonicKeys;
-module.exports = networkURL;
+module.exports = { mnemonicKeys, networkURL };
